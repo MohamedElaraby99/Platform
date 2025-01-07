f231a4f46fa693178a7079427a1ef3548c494ed9
Hazem <hazem77mostafa90@gmail.com>
2025-01-07 02:57:01 +0200

    add auth apis

---
+++ BackEnd/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require("../models/User");
+
+const getAllUsers = async (req, res) => {
+  const users = await User.find().select("-password").lean();
+
+  if (users.length === 0) {
+    return res.status(400).json({ message: "there's no users" });
+  }
+  res.json(users);
+};
+
+module.exports = {
+  getAllUsers,
+};
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,49 @@
-const express  = require("")
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/dbConnect");
+const allowedOrigins = require("./config/allowedOrigins");
+const corsOptions = require("./config/corsOptions");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path")
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+connectDB();
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "public")))
+console.log(path.join(__dirname, ".", "views", "404.html"));
+
+app.use("/", require("./routes/root"))
+app.use("/auth", require("./routes/authRoutes"))
+app.use("/users", require("./routes/userRoutes"))
+
+app.all("*", (req, res)=>{
+    res.status(404)
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, ".", "views", "404.html"))
+    }else if(req.accepts("json")){
+        res.json({message: "404 Not Found"})
+    }else{
+        res.type("txt").send("404 Not Found")
+    }
+})
+
+mongoose.connection.once("open", () => {
+  console.log("Connect on MongoDB");
+  app.listen(PORT, () => {
+    console.log(`server runing on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+
