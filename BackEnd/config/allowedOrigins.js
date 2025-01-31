0efbecaf415447b057f851e33dc3afae4fa77b68
Hazem <hazem77mostafa90@gmail.com>
2025-02-01 00:19:51 +0200

    allow origin

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -1,9 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://tawakol.live:8000",
-  "https://tawakol.live:8000",
-  "http://185.97.146.230:8000",
-  "https://185.97.146.230:8000",
-];
+const allowedOrigins = ["http://localhost:3000"];
 
 module.exports = allowedOrigins;
