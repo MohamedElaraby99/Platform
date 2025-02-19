3c82c0ea4c81c2b8f0439c1ccc43e46116232b4c
root <root@srv710634.hstgr.cloud>
2025-02-19 15:35:48 +0000

    1

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://tawakol.live", "http://localhost:3000"];
+const allowedOrigins = ["https://tawakol.live","https://www.tawakol.live", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
